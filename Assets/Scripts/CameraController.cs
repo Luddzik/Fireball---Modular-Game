71d312fe3bcfaed19070d932baa5cf31142b08fb
Ludwik Bacmaga <luddzik@gmail.com>
2017-03-20 10:09:21 +0000

    Modular game components and project
    Created a modular game with multi-directional movement and random
    fireball shooting. Avoid the fireballs.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float cameraHeight = 10.0f;
+	public float cameraAway = 5.0f;
+
+	void Update() {
+		Vector3 pos = player.transform.position;
+		pos.y += cameraHeight;
+		pos.x -= cameraAway;
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/Projectiles.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour {
+	public GameObject projectile; 
+
+	public Transform[] SpawnProjectile;
+
+	public float time = 0.0f;
+
+	float counter;
+
+	void Start() {
+		counter = time;
+	}
+
+	void Update() {
+
+		counter -= Time.deltaTime;
+
+		if (counter <= 0) {
+
+			counter = time;
+
+			Transform shootingPoint = ShootPoint ();
+
+			var positionX = shootingPoint.transform.position.x;
+
+			var positionY = 1.6f;
+
+			var randomZ = Random.Range(-360,360);
+
+			Vector3 randomDir = new Vector3(positionX,positionY,randomZ);
+
+			GameObject bullet = Instantiate (projectile, shootingPoint.position, Quaternion.identity) as GameObject;
+			//bullet.GetComponent<Rigidbody> ().AddForce (transform.forward * 10);
+			bullet.GetComponent<Rigidbody>().AddForce(randomDir * 5);
+		}
+	}
+
+	Transform ShootPoint() {
+		var number = Random.Range (0, SpawnProjectile.Length);
+		return SpawnProjectile [number].transform;
+	}
+}
